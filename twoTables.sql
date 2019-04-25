cb369cef98268897bd87365a4b6726d6c3c235c1
Ayr2017 <ayrat.2013@yandex.ru>
2019-04-25 17:19:23 +0300

    Completed two queries

--- twoTables.sql
+++ twoTables.sql
@@ -17,7 +17,7 @@ CREATE TABLE `department` (
 PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-USE php02;
+/* USE php02;
 SHOW TABLES;
 
 INSERT INTO department values( 1, 'bookkeeping'),
@@ -32,9 +32,14 @@ SELECT department_id, COUNT(*)
 
  SELECT department.name AS dep_name, department.id AS dep_id, worker.firstname AS worker_name FROM
     department INNER JOIN worker ON  department.id = worker.department_id;
-
-
+ */
+--first query----
 SELECT A.dep_name, COUNT(dep_id) FROM
     ( SELECT department.name AS dep_name, department.id AS dep_id FROM
+    department INNER JOIN worker ON  department.id = worker.department_id
+    ) AS A GROUP BY dep_id HAVING COUNT(dep_id) >= 5;
+--second query ----
+SELECT A.dep_name, GROUP_CONCAT(worker_id) FROM
+    ( SELECT department.name AS dep_name, department.id AS dep_id,  worker.id AS worker_id FROM
     department INNER JOIN worker ON  department.id = worker.department_id
     ) AS A GROUP BY dep_id HAVING COUNT(dep_id) >= 5;
